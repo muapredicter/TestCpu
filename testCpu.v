e3c1db40146f044b5496264c9fd02703b4f2b5d2
muapredicter <2816137428@qq.com>
2025-02-25 11:28:29 +0800

    增加J型指令的指令码

--- testCpu.v
+++ testCpu.v
@@ -6,8 +6,6 @@
 `define Valid 1'b1
 `define Invalid 1'b0
 
-`define Nop 6'b000000
-
 // ID begin
 // I型指令
 `define Inst_ori 6'b001101
@@ -29,20 +27,37 @@
 `define Inst_sra 6'b000011 
 
 // J型指令
-
+`define Inst_j   6'b000110
+`define Inst_jal 6'b000101
+`define Inst_jr  6'b001000
+`define Inst_jalr 6'b001001
+`define Inst_beq  6'b010000
+`define Inst_bne  6'b010001
+`define Inst_bltz 6'b010010
+`define Inst_bgtz 6'b010011
 // ID end
 
 // Ex操作
-`define Or 6'b001101
-`define And 6'b001100
-`define Xor 6'b001110
-`define Add 6'b001000
-`define Sub 6'b001001
-`define Lui 6'b001111
-`define Sll 6'b000000
-`define Subr 6'b100010
-`define Srl 6'b000010
-`define Sra 6'b000011
+`define Nop 6'b000000 
+`define Or  6'b000001
+`define And 6'b000010
+`define Xor 6'b000011
+`define Add 6'b000100
+`define Sub 6'b000101
+`define Lui 6'b100000
+
+`define Sll 6'b000110
+`define Srl 6'b000111
+`define Sra 6'b001000
+
+`define J   6'b001001
+`define Jal 6'b001010
+`define Jr  6'b001011
+
+`define Beq 6'b001100
+`define Bne 6'b001101
+`define Bgtz 6'b001110
+`define Bltz 6'b001111
 
 
 module IF(
